31dab02e58cad56e41c32847edbf79a8adc885ad
tomekl007 <tomekl007@gmail.com>
2013-10-25 22:03:19 +0200

    first simple streaming using html5 video, websoctets and node js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express'),http = require('http');
 var app = express();
+app.use(express.static(__dirname))
 var server = http.createServer(app);
+var path = require('path');
 server.listen(8080);
+
 var io = require("socket.io").listen(server);
 io.sockets.on('connection',function(socket){
 	socket.on('newFrame',function(img){
+		console.log("--->on newFrame")
 		io.sockets.emit('setFrame',img);
 	});
 });
